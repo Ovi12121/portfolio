e9c683011d25ffd581e70854e9aba366bae06c57
Ovi12121 <136064592+Ovi12121@users.noreply.github.com>
2026-07-23 22:23:21 +0600

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -142,6 +142,13 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     links: [
       { rel: "canonical", href: "https://shahedulislamovi.com" },
       { rel: "stylesheet", href: appCss },
+       {
+  rel: "preload",
+  href:
+    "https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600;700&family=Inter:wght@400;500;600&display=swap",
+  as: "style",
+  crossOrigin: "anonymous",
+},
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
