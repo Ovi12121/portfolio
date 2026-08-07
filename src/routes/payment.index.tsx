494b76642cd7979a7cf66dbfbc5709a5101171aa
Ovi12121 <136064592+Ovi12121@users.noreply.github.com>
2026-08-07 17:42:22 +0600

    Update payment.index.tsx

--- src/routes/payment.index.tsx
+++ src/routes/payment.index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/payment")({
+export const Route = createFileRoute("/payment/")({
   head: () => ({
     title: "Monthly Automation Support Plans | Shahedul Islam Ovi",
     meta: [
