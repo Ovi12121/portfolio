b61460742f2e01abff24da028fe32968372f6262
Ovi12121 <136064592+Ovi12121@users.noreply.github.com>
2026-08-09 23:16:30 +0600

    Update payment.tsx

--- src/routes/payment.tsx
+++ src/routes/payment.tsx
@@ -121,7 +121,7 @@ export function PaymentPage() {
 
               <div className="mt-8">
                 <a
-                  href="https://oviautomation.gumroad.com/l/starter/buy"
+                  href="https://oviautomation.gumroad.com/l/starter?wanted=true&single=true"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="btn-outline w-full text-center block !py-3"
@@ -172,7 +172,7 @@ export function PaymentPage() {
 
               <div className="mt-8">
                 <a
-                  href="https://oviautomation.gumroad.com/l/ezjten/buy"
+                  href="https://oviautomation.gumroad.com/l/ezjten?wanted=true&single=true"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="btn-primary w-full text-center block !py-3"
